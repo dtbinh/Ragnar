c0b74e97d38b60fa247384918d9ce2f8ecb6ef6b
BBS007 <bbs07@laposte.net>
2016-02-09 12:03:01 +0100

    Ajout des EventType

---
+++ Ragnar/src/fr/isima/sma/simulator/events/EventType.java
@@ -0,0 +1,9 @@
+package fr.isima.sma.simulator.events;
+
+public enum EventType {
+	Fight, 		// Combats
+	Robery, 	// Braquage
+	Release, 	// Liberation
+	Move, 		// Deplacements
+	Idle 		// Ne rien faire / dormir
+}
